175ef3a2533e8e49949e8ec26471aed89a7a0d6d
emilio.martinez <emilio.martinez@PT-0041>
2026-03-04 08:12:46 +0100

    Agregar archivos de proyecto.

---
+++ Articulo.cs
@@ -0,0 +1,20 @@
+﻿namespace Articulos_Backend
+{
+    public class Articulo
+    {
+        public int id {  get; set; }
+
+        public String nombre { get; set; }
+
+        public double precio { get; set; }
+
+        public String categoria { get; set; }
+
+        public Articulo(String nombre, double precio, String categoria)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.categoria = categoria;
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿namespace Articulos_Backend;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public String? Summary { get; set; }
+
+    public WeatherForecast(DateOnly fecha, int temperatura, String resumen)
+    {
+        this.Date = fecha;
+        this.TemperatureC = temperatura;
+        this.Summary = resumen;
+    }
+}
