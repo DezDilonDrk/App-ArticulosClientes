1a4192c8776ceb0c99e90ec51b53236624562da3
leandrobsantim-cyber <leandrobsantim@gmail.com>
2026-03-04 09:14:19 +0100

    fusionar proyectos, segundo commit

--- Program.cs
+++ Program.cs
@@ -4,28 +4,13 @@ using ClientesASPNET;
 using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-// OpenAPI is preserved
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
-
-
-
 app.MapClienteEndpoints();
-
 app.MapArticuloEndpoints();
-
-
-
 app.Run();
