ba116f139e529b47e13c40fb05c47fa4daa4f74d
leandrobsantim-cyber <leandrobsantim@gmail.com>
2026-03-10 13:14:37 +0100

    inicio de fechas en Cliente

--- Backend/Clientes/Cliente.cs
+++ Backend/Clientes/Cliente.cs
@@ -1,22 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace ClientesASPNET
+namespace ClientesASPNET;
+
+public class Cliente
 {
-    public class Cliente
-    {
-        public string Dni { get; set; }
-        public string Nombre { get; set; }
-        public string Apellidos { get; set; }
-        [EmailAddress(ErrorMessage = "Correo inválido")]
-        public string Email { get; set; }
+    public string Dni { get; set; }
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+    [EmailAddress(ErrorMessage = "Correo inválido")]
+    public string Email { get; set; }
+    public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    public DateTime FechaModificacion { get; set; } = DateTime.Now;
+    public Cliente() { }
 
-        public Cliente() { }
-
-        public Cliente(string dni, string nombre, string apellidos, string email) {
-            this.Dni = dni;
-            this.Nombre = nombre;
-            this.Apellidos = apellidos;
-            this.Email = email;
-        }
+    public Cliente(string dni, string nombre, string apellidos, string email, DateTime) {
+        this.Dni = dni;
+        this.Nombre = nombre;
+        this.Apellidos = apellidos;
+        this.Email = email;
     }
 }
