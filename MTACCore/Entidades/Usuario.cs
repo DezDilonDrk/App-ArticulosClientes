6cfb5dc5c99c3b2487d8bb9efeb55e4031b7aea8
emilio.martinez <emilio.martinez@PT-0041>
2026-03-26 14:23:10 +0100

    Endpoints, modelo  y repositorio de Usuario

---
+++ MTACCore/Entidades/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTCore_AC.Entidades
+{
+    public class Usuario
+    {
+        public string Correo { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
+        public string Rol { get; set; }
+
+        public Usuario() { }
+
+        public Usuario(string correo, string nombre, string contrasena, string rol)
+        {
+            this.Correo = correo;
+            this.Nombre = nombre;
+            this.Contrasena = contrasena;
+            this.Rol = rol;
+        }
+
+    }
+}
