ce90ebc3c8456fcd2bce3bac412667f4896a16d5
emilio.martinez <emilio.martinez@PT-0041>
2026-04-14 10:29:08 +0200

    cambio de nombres en variables de roles

--- Backend/JWT/Roles.cs
+++ Backend/JWT/Roles.cs
@@ -2,8 +2,8 @@
 
 public static class Roles
 {
-    public const string AdminArticulos = "ADMIN_ARTICULOS";
-    public const string UserArticulos = "USER_ARTICULOS";
+    public const string AdminAlmacen = "ADMIN_ALMACEN";
+    public const string UserAlmacen = "USER_ALMACEN";
     public const string AdminVentas = "ADMIN_VENTAS";
     public const string UserVentas = "USER_VENTAS";
     public const string AdminPedidos = "ADMIN_PEDIDOS";
