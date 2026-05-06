1b4209c62375097cca711214f5568a3cec9ca9bb
leandrobsantim-cyber <leandrobsantim@gmail.com>
2026-05-06 08:36:12 +0200

    ultimo commit antes del merge

--- Frontend/SesionMT/UserSession.cs
+++ Frontend/SesionMT/UserSession.cs
@@ -3,6 +3,6 @@
     public static class UserSession
     {
         private static HttpClient client = new HttpClient();
-        public static string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoibGVhbmRyby5zYW50aWxhcmlvQG10aGVsbWV0cy5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsiQURNSU5fQUxNQUNFTiIsIkFETUlOX1ZFTlRBUyIsIkFETUlOX1NFR1VSSURBRCJdLCJleHAiOjE3Nzc5ODU4MTl9.6pc3rVNW4Mntg7Sr1DSEhJsFER8znIsXitgcSAgYJQk"; //Sustituir lo del front por esto, para usarlo aquí y en Test
+        public static string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoibGVhbmRyby5zYW50aWxhcmlvQG10aGVsbWV0cy5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsiQURNSU5fQUxNQUNFTiIsIkFETUlOX1ZFTlRBUyIsIkFETUlOX1NFR1VSSURBRCJdLCJleHAiOjE3NzgwNTIwNzR9.ONVuEMUGt8AEHm2P862L-b1fKe59sIXR3njUDJrfXRI"; //Sustituir lo del front por esto, para usarlo aquí y en Test
     }
 }
