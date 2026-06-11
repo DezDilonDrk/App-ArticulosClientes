30674df3892f20f9f9b9d032160e3bb23e0f6cb3
leandrobsantim-cyber <leandrobsantim@gmail.com>
2026-06-11 14:34:36 +0200

    Creación de un nuevo PAK (aún falta hacerle pruebas)

---
+++ MTNegocios/Scripts/PAK_2026611000000_ConstraintUniqueUsuarioEmail.cs
@@ -0,0 +1,12 @@
+﻿using MTCore_AC.DTO;
+
+namespace MTNegocios.Scripts
+{
+    public class PAK_2026611000000_ConstraintUniqueUsuarioEmail: Script
+    {
+        public PAK_2026611000000_ConstraintUniqueUsuarioEmail()
+        {
+            this.script = "ADD CONSTRAINT UQ_Usuarios_CorreoElectronico UNIQUE (CorreoElectronico)"; //A lo mejor esto falla, es para hacer pruebas
+        }
+    }
+}
